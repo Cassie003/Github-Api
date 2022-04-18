bd17b296800129c59518f3af1f92457642c63a5c
Cassie003 <caseyminu@gmail.com>
2022-04-18 22:07:24 +0300

    Add user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  personal: any;
+  user: string;
+  repos: any;
+
+
+  constructor(private githubservice: GithubService) {
+
+  }
+  
+
+
+
+  findUser() {
+    this.githubservice.findUserName(this.user)
+    
+
+    this.githubservice.getPersonalInfo().subscribe(personal => {
+
+      this.personal = personal;
+
+
+      
+    });
+    this.githubservice.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }  
+  ngOnInit(): void {
+  }
+
+}
