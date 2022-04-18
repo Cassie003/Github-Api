e21aabc8d188effd9c80812ea8de502247626f81
Cassie003 <caseyminu@gmail.com>
2022-04-18 21:51:07 +0300

    Initial Commit

---
+++ src/app/github.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  private username: string;
+  private clienttoken!: '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';
+
+
+
+  constructor(private httpClient: HttpClient) {
+  
+    this.username = "Cassie003";
+
+  }
+  getPersonalInfo() {
+    return this.httpClient.get('https://api.github.com/users/'+this.username + "?client_token=" + this.clienttoken)
+    .map((res: any) => {
+      return res
+  });
+}
+
+getRepos(){
+  return this.httpClient.get('https://api.github.com/users/'+this.username + "/repos?client_token=" + this.clienttoken)
+  .map((res: any) => {
+    return res
+});
+}
+
+findUserName(name: string){
+  this.username=name
+}
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  personal: any;
+  user: string | undefined;
+  repos: any;
+
+
+  constructor(private githubservice: GithubService) {
+
+    
+
+  }
+  
+
+
+
+  findUser() {
+    this.githubservice.findUserName(this.user)
+    
+
+    this.githubservice.getPersonalInfo().subscribe((personal: any) => {
+
+      this.personal = personal;
+
+
+      
+    });
+    this.githubservice.getRepos().subscribe((repos: any) => {
+      console.log(repos);
+      this.repos=repos;
+    });
+
+  }  
+
+  ngOnInit(): void {
+  }
+
+}
