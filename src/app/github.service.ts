aae321b312bc0d730e18df9ef1ba86095d540394
Cassie003 <caseyminu@gmail.com>
2022-04-18 23:26:56 +0300

    Fix all import error

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,26 +8,26 @@ import 'rxjs/add/operator/map';
 export class GithubService {
 
   private username: string;
-  private clienttoken: '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';
+  private clienttoken: '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';
 
 
   constructor(private httpClient: HttpClient) {
   
-    this.username = "TERESIA012";
+    this.username = "Cassie003";
 
   }
   getPersonalInfo() {
     return this.httpClient.get('https://api.github.com/users/'+this.username + "?client_token=" + this.clienttoken)
-    .map((res: any) => {
+    .pipe(map((res: any) => {
       return res
-  });
+    }));
 }
 
 getRepos(){
   return this.httpClient.get('https://api.github.com/users/'+this.username + "/repos?client_token=" + this.clienttoken)
-  .map((res: any) => {
+  .pipe(map((res: any) => {
     return res
-});
+}));
 }
 
 findUserName(name: string){
