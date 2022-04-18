0089ee4869424ce28b6551427fbcebe70d68563f
Cassie003 <caseyminu@gmail.com>
2022-04-18 22:05:00 +0300

    Add home component

---
+++ src/app/home/dist/home.component.js
@@ -0,0 +1,37 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.HomeComponent = void 0;
+var core_1 = require("@angular/core");
+var HomeComponent = /** @class */ (function () {
+    function HomeComponent(githubservice) {
+        this.githubservice = githubservice;
+    }
+    HomeComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.user = "Cassie003";
+        this.githubservice.findUserName(this.user);
+        this.githubservice.getPersonalInfo().subscribe(function (personal) {
+            console.log(personal);
+            _this.personal = personal;
+        });
+        this.githubservice.getRepos().subscribe(function (repos) {
+            console.log(repos);
+            _this.repos = repos;
+        });
+    };
+    HomeComponent = __decorate([
+        core_1.Component({
+            selector: 'app-home',
+            templateUrl: './home.component.html',
+            styleUrls: ['./home.component.css']
+        })
+    ], HomeComponent);
+    return HomeComponent;
+}());
+exports.HomeComponent = HomeComponent;
